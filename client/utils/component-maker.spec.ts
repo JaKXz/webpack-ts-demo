2d5335fafa5454ffb8bcb6e64b289a31d31b1a17
Jason Kurian <jasonk92@gmail.com>
2015-11-12 00:06:02 -0500

    First passing test on component-maker

--- client/utils/component-maker.spec.ts
+++ client/utils/component-maker.spec.ts
@@ -1,5 +1,4 @@
 import {makeDirective, makeSelector} from './component-maker';
-import {expect} from 'chai'
 
 describe('Component Maker', () => {
   describe('Make directive', () => {
@@ -9,6 +8,13 @@ describe('Component Maker', () => {
   });
 
   describe('Make selector', () => {
+    it('should return an unmodified one word selector', () => {
+      class MainComponentMock {
+        public static selector: string = 'main';
+      }
+      expect(makeSelector(MainComponentMock)).to.equal('main');
+    });
+
     it('should return a camelCased selector for a two-word selector');
 
     it('should return a valid multipleCamelCased for a three-word or more selector');
